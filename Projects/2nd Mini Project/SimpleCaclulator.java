6c5f39822fd5b6a4563e3923a1adf20f6da9622d
Rajat-Builds <rajatsingh12154@gmail.com>
2026-04-10 20:36:00 +0530

    Add Simple Calculator mini project

---
+++ Projects/2nd Mini Project/SimpleCaclulator.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class SimpleCaclulator {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter 1st Number: ");
+        double num1 = sc.nextDouble();
+
+        System.out.println("Enter an operator (+, -, *, /): ");
+        char operator = sc.next().charAt(0);
+
+        System.out.println("Enter 2nd Number: ");
+        double num2 = sc.nextDouble();
+
+        double result;
+
+        if (operator == '+') {
+            result = num1 + num2;
+            System.out.println("Result: " + result);
+        } else if (operator == '-') {
+            result = num1 - num2;
+            System.out.println("Result: " + result);
+        } else if (operator == '*') {
+            result = num1 * num2;
+            System.out.println("Result: " + result);
+        } else if (operator == '/') {
+            if (num2 != 0) {
+                result = num1 / num2;
+                System.out.println("Result: " + result);
+            } else {
+                System.out.println("Error: Cannot divide by zero.");
+            }
+        } else {
+            System.out.println("Error: Invalid operator.");
+        }
+        sc.close();
+    }
+}
