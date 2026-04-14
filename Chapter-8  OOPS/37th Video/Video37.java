27ad00f117de064ce1f676d7c6ca34db93cfb4cc
Rajat-Builds <rajatsingh12154@gmail.com>
2026-04-14 11:34:15 +0530

    Added file Video37 Terminologies in OOPs

---
+++ Chapter-8  OOPS/37th Video/Video37.java
@@ -0,0 +1,10 @@
+// In this file , i learned about basic terminilogies in Obeject oriented programming . 
+
+// Terminilogies i have learned :-
+
+// 1. Abstraction
+// 2. Encapsulation
+// 3. Inheritance
+// 4. Polymorphism
+
+/* For more detailed notes , please refere notes section .  */
