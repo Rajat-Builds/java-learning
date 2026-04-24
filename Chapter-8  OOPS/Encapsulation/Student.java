3526387ebaedfb03aadaa1341502a1a739cab237
Rajat-Builds <rajatsingh12154@gmail.com>
2026-04-24 12:53:49 +0530

    Added and learned about Encapsulation

--- Chapter-8  OOPS/Encapsulation/Student.java
+++ Chapter-8  OOPS/Encapsulation/Student.java
@@ -1,3 +1,7 @@
+// Encapsulation means making your variables private and controlling access to them through getters and setters, so that invalid data can never get into your object. // 
+
+
+
 public class Student {
     private String name;
     private int age;
@@ -16,7 +20,7 @@ public class Student {
     }
 
     public void setName(String name) {
-        if (name.isEmpty() || name == null) {
+        if (name.isEmpty() || name == null) { // to check if a String is empty we use - name.isEmpty() and to check null we use - name == null // 
             System.out.println("Invalid name. ");
         } else {
             System.out.println(this.name = name);
