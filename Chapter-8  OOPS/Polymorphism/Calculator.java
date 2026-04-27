d88e8dd2486ed5134b04777c4c4bf9b09e4fe691
Rajat-Builds <rajatsingh12154@gmail.com>
2026-04-27 12:58:22 +0530

    Added and learned about Polymorphism

--- Chapter-8  OOPS/Polymorphism/Calculator.java
+++ Chapter-8  OOPS/Polymorphism/Calculator.java
@@ -1,4 +1,10 @@
-/*  Polymorphism: Polymorphism means one thing behaving differently in different situations , Simply means many forms . */
+/*  Polymorphism: Polymorphism means one thing behaving differently in different situations , Simply means many forms . 
+
+Method Overriding: A child class can redefine a method of its parent class.
+
+Method Overloading: We can define multiple methods with the same name but different parameters.
+
+*/
 
 public class Calculator {
 
