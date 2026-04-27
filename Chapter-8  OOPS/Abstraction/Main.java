911d362cb0cfcbd1f51d78599580a57ca33c7bec
Rajat-Builds <rajatsingh12154@gmail.com>
2026-04-27 19:45:08 +0530

    Added and learned about Abstraction

---
+++ Chapter-8  OOPS/Abstraction/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Circle c1 = new Circle(5);
+        System.out.println("Area is " + c1.area());
+
+        Rectangle r1 = new Rectangle(4, 5);
+        System.out.println("Area is " + r1.area());
+    }
+}
---
+++ Chapter-8  OOPS/Abstraction/Shape.java
@@ -0,0 +1,6 @@
+/* Abstraction in Java is the process of hiding complex internal details and showing only the essential features to the user. In simple words, it focuses on what an object does rather than how it does it. */ 
+
+public abstract class Shape {
+
+    public abstract double area();
+}
