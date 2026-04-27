1f16c38abcfcbbf854e72fb2f759f4da5f03813b
Rajat-Builds <rajatsingh12154@gmail.com>
2026-04-27 19:58:38 +0530

    Added and learned about Interface

---
+++ Chapter-8  OOPS/Interfaces/Flyable.java
@@ -0,0 +1,6 @@
+/* Interface in Java is a contract or a blueprint that tells a class what it must do, but not how to do it. */
+
+public interface Flyable {
+
+    void fly();
+}
---
+++ Chapter-8  OOPS/Interfaces/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        Duck d1 = new Duck("Donald");
+        d1.fly();
+        d1.swim();
+    }
+}
