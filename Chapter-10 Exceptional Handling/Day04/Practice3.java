57da99fe3aa56df244c17e4d42b0cc23c4a658ae
Rajat-Builds <rajatsingh12154@gmail.com>
2026-05-27 10:15:23 +0530

    Update Chapter 10 - Exceptional Handling

--- Chapter-10 Exceptional Handling/Day04/Practice3.java
+++ Chapter-10 Exceptional Handling/Day04/Practice3.java
@@ -4,13 +4,15 @@ public class Practice3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Two Numbers: ");
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
+        String num1 = sc.nextLine();
+        String num2 = sc.nextLine();
         try {
-            int result = num1 / num2;
+            int input1 = Integer.parseInt(num1);
+            int input2 = Integer.parseInt(num2);
+            int result = input1 / input2;
             System.out.println("Result: " + result);
         } catch (ArithmeticException e) {
-            System.out.println("Error: Cannot devided my zero.");
+            System.out.println("Error: Cannot devided by zero.");
         } catch (NumberFormatException e) {
             System.out.println("Error: Please enter valid numeric integers.");
         } finally {
