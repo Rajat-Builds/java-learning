ce9bf8386cd5f45b627b5bd3f7501fdedf36fbcf
Rajat-Builds <rajatsingh12154@gmail.com>
2026-06-03 08:32:17 +0530

    Add POST requests, RequestBody, RequestParam endpoints

--- Chapter-12 Spring Boot/studentapi/src/main/java/com/rajat/studentapi/StudentController.java
+++ Chapter-12 Spring Boot/studentapi/src/main/java/com/rajat/studentapi/StudentController.java
@@ -3,11 +3,17 @@ package com.rajat.studentapi;
 import java.util.ArrayList;
 import java.util.List;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class StudentController {
+
+    List<Student> students = new ArrayList<>();
     @GetMapping("/student")
     public Student getStudent() {
         Student s = new Student("Rajat", 20, 85.5);
@@ -16,10 +22,24 @@ public class StudentController {
 
     @GetMapping("/students")
     public List<Student> getAllStudents() {
-        List<Student> students = new ArrayList<>();
-        students.add(new Student("Rajat", 20, 85.5));
-        students.add(new Student("Peehu", 19, 95.5));
-        students.add(new Student("Krishna", 21, 99.9));
         return students;
     }
+
+    @PostMapping("/students/add")
+    public String addStudent(@RequestBody Student student) {
+        students.add(student);
+        return "Student added: " + student.getName();
+    }
+
+    @GetMapping("/students/search")
+    public String searchStudent(@RequestParam String name) {
+        for (Student student : students){
+            if (student.getName().equals(name)) {
+                return student.getName() + " found!";
+            }
+        }
+        return "Student not found";
+    }
+
+
 }
