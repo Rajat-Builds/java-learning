af877d9f82da55169c1fc76754fea70e5070fcb8
Rajat-Builds <rajatsingh12154@gmail.com>
2026-07-04 22:22:42 +0530

    Remove Practice Sets Folder

--- Practice Sets/Practice Set-3/question1.java
+++
@@ -1,9 +0,0 @@
-public class question1 {
-    public static void main(String[] args) {
-        // quesiton -1
-
-        String name = "Rajat";
-        System.out.println(name.toLowerCase());
-
-    }
-}
--- Practice Sets/Practice Set-3/question3.java
+++
@@ -1,9 +0,0 @@
-public class question3 {
-    public static void main(String[] args) {
-        // question-3
-
-        String letter = "Dear <|name|>, Thanks a lot !";
-        letter = letter.replace("<|name|>", "Rajat");
-        System.out.println(letter);
-    }
-}
