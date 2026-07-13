feb229f6572a08720cde3e4f7d6315537a7a4084
Rajat-Builds <rajatsingh12154@gmail.com>
2026-07-13 16:40:58 +0530

    Day07 - Add Bubble Sort with optimization

---
+++ DSA/Day 07/Practice.java
@@ -0,0 +1,26 @@
+
+import java.util.Arrays;
+
+public class Practice {
+    public static void bubbleSort(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 5, 2, 8, 1, 9, 3 };
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
